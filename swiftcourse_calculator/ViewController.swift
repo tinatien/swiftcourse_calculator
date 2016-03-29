43a99a058aa27c5d8f388aba43272583c0f75bbe
TinaTien <tinajay8427@gmail.com>
2016-03-29 20:22:19 +0800

    fix changeOperate function

--- swiftcourse_calculator/ViewController.swift
+++ swiftcourse_calculator/ViewController.swift
@@ -18,7 +18,8 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated. 
+        
     }
     
     
